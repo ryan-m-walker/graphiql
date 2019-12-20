dc23f89d49d1b200c4f9784c96b8251aae631ae5
Ryan Walker <ryan.matthew.walker@gmail.com>
2019-12-19 23:29:58 -0500

    feat: replace use of enzyme with react-testing-library
    - added @testing-library/react to graphiql package
    - added @testing-libary/jest-dom to graphiql package
    - removed usage of ENZYM env variable in test script in graphiql packaget
    - removed enzyme adapter setup from enzyme.config.js file and renamed it
    to test.config.js
    - removed use of enzyme and replaced with react-testing-library in all
    graphiql package test files and updated tests to use
    react-testing-library patterns
    - moved mockStorage to own file so it can be reused in other test files
    and added other methods to it
    - added a CodeMirror mock to be used in tests
    - mocked out codemirror addon modules

---
+++ packages/graphiql/src/components/__tests__/helpers/storage.js
@@ -0,0 +1,27 @@
+export function getMockStorage() {
+  return (function() {
+    let store = {};
+    return {
+      getItem(key) {
+        return store.hasOwnProperty(key) ? store[key] : null;
+      },
+      setItem(key, value) {
+        store[key] = value.toString();
+      },
+      clear() {
+        store = {};
+      },
+      removeItem(key) {
+        if (store.hasOwnProperty(key)) {
+          const updatedStore = {};
+          for (const k in store) {
+            if (k !== key) {
+              updatedStore[k] = store[k];
+            }
+          }
+          store = updatedStore;
+        }
+      },
+    };
+  })();
+}
